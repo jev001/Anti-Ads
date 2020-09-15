7cdc16845ccd883c4c8b80d9be949f34e8a8d0a6
jonah <taojing110>
2020-09-15 12:54:50 +0800

    update log

--- userscript/javaweb_shop_ad.user.js
+++ userscript/javaweb_shop_ad.user.js
@@ -12,7 +12,9 @@
     var groups = p.exec(requestUrl).groups
     if(groups.id){
         var downloadCacheStr = localStorage.getItem("_ARTICLE_DOWNLOAD_INFO_CACHE_")
+        console.debug("origin [_ARTICLE_DOWNLOAD_INFO_CACHE_= "+downloadCacheStr+" ]")
         localStorage.setItem("_ARTICLE_DOWNLOAD_INFO_CACHE_","{"+groups.id+":true}")
+        console.info("success replace _ARTICLE_DOWNLOAD_INFO_CACHE_")
     }else{
         console.warn("error get article id from ["+requestUrl+"]")
     }
